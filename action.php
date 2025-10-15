5b757366cade9083ee672d759198fa0e1fd6c4bf
jayram-uiux <a.jayaram@edgezenlabs.com>
2025-10-15 15:46:14 +0530

    form-function-1

---
+++ action.php
@@ -0,0 +1,35 @@
+<?php
+    $errors = '';
+    $myemail = 'a.jayaram@edgezenlabs.com';//<-----Put Your email address here.
+    if(empty($_POST['name'])  ||
+       empty($_POST['phonenumber']) ||
+       empty($_POST['loanamountrequired']) ||
+       empty($_POST['message']))
+    {
+        $errors .= "\n Error: all fields are required";
+    }
+    $name = $_POST['name'];
+    $email_address = $_POST['phonenumber'];
+    $email_address = $_POST['loanamountrequired'];
+    $message = $_POST['message'];
+    if (!preg_match(
+    "/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i",
+    $email_address))
+    {
+        $errors .= "\n Error: Invalid email address";
+    }
+    
+    if( empty($errors))
+    {
+    $to = $myemail;
+    $email_subject = "Contact form submission: $name";
+    $email_body = "You have received a new message. ".
+    " Here are the details:\n Name: $name \n ".
+    "Email: $email_address\n Message \n $message";
+    $headers = "From: $myemail\n";
+    $headers .= "Reply-To: $email_address";
+    mail($to,$email_subject,$email_body,$headers);
+    //redirect to the 'thank you' page
+    header('Location: contact-form-thank-you.html');
+    }
+    ?>
